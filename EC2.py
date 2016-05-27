dde851a251fb59551f3e8d14bc3fe375d73f64d5
Kavithamugund <kavithamugund@gmail.com>
2016-05-26 23:45:18 -0400

    Create EC2.py

---
+++ EC2.py
@@ -0,0 +1,46 @@
+from boto.ec2.connection import EC2Connection
+from settings import AWS
+
+import time
+
+
+SERVER = {
+		'image_id' : AWS['defaults']['image_id'],
+		'instance_type' : AWS['defaults']['instance_type'],
+		'security_groups' : AWS['defaults']['security_groups'],
+		'key_name' : AWS['defaults']['key_name'],
+}
+
+
+class EC2Conn:
+
+	def __init__(self):
+		self.conn = None
+
+
+	def connect(self):
+		self.conn = EC2Connection(AWS['secrets']['aws_key'],
+								  AWS['secrets']['aws_secret'])
+
+	def create_instance(self):
+		reservation = self.conn.run_instances( **SERVER)
+		print reservation
+		instance = reservation.instances[0]
+		time.sleep(10)
+		while instance.state != 'running':
+			time.sleep(5)
+			instance.update()
+			print "Instance state: %s" % (instance.state)
+		
+		# Sleep for a bit more before trying to connect
+		time.sleep(60)
+
+		print "instance %s done!" % (instance.id)
+
+		return instance
+
+
+def create_new_instance():
+	a = EC2Conn()
+	a.connect()
+	return a.create_instance()
